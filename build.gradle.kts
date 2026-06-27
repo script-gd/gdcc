e6c2d2cde435737b7b9750a2d5c2a6753acdf124
Iridium-Zero <mail@ir0.io>
2026-06-28 00:21:25 +0800

    chore(build): release 0.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "gd.script"
-version = "0.0.1"
+version = "0.0.2"
 
 val generatedVersionResources = layout.buildDirectory.dir("generated/resources/gdccVersion")
 val runtimeLibDir = layout.buildDirectory.dir("libs/lib")
